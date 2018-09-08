17c7b9fc1c72df0c52b2ed16c8ed179ee426cef9
蔡忠翊 <mac@caizhongyideMacBook-Pro.local>
2018-09-08 18:52:34 +0800

    add designers resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "products#index"
+  #建立設計師館頁面
+  resources :designers
 end
