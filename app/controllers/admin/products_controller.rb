4b250182787f9e5cd88a44c40b0f9740ac95f59f
gasper <bricktowntw@gmail.com>
2018-09-08 18:20:52 +0800

    add products index action and view for admin

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,5 @@
+class Admin::ProductsController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :products, only: [:index, :show]
   root "products#index"
+
+  namespace :admin do
+    root "products#index"
+  end
+
+
+
 end
