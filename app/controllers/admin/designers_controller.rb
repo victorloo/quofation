5a0542908d9d14170f4987634d32ddb661e148df
jackal <hata.s520@gmail.com>
2018-09-08 20:45:27 +0800

    add designer new and create action and view for admin

--- app/controllers/admin/designers_controller.rb
+++ app/controllers/admin/designers_controller.rb
@@ -2,4 +2,25 @@ class Admin::DesignersController < ApplicationController
   def index
     @designers = Designer.all
   end
+
+  def new
+    @designer = Designer.new
+  end
+
+  def create
+    @designer = Designer.new(designer_params)
+    if @designer.save
+      flash[:notice] = "成功新增設計師"
+      redirect_to admin_designers_path
+    else
+      flash.now[:alert] = "新增設計師失敗"
+      render :new
+    end    
+  end
+
+  private
+
+  def designer_params
+    params.require(:designer).permit(:name, :brandname, :description, :image)
+  end
 end
--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,2 +1,3 @@
 class Designer < ApplicationRecord
+  validates_presence_of :name
 end
