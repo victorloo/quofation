b079fe93373547c72cb22b90c6cb5dba32b05938
gasper <bricktowntw@gmail.com>
2018-09-08 21:31:21 +0800

    set up view for product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @products = Product.page(params[:page]).per(9)
   end
   
 end
