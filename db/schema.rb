d15aa16652a1b5f788e388f7bf987f254e2ca35b
jackal <hata.s520@gmail.com>
2018-09-08 19:07:29 +0800

    create designer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_203430) do
+ActiveRecord::Schema.define(version: 2018_09_08_110156) do
+
+  create_table "designers", force: :cascade do |t|
+    t.string "name"
+    t.string "brandname"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
