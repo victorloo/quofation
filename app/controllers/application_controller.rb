1fcf7f6814e2e07e2286b7c28f9ed3031eae3c76
gasper <bricktowntw@gmail.com>
2018-09-11 10:34:37 +0800

    build up cart model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::Base
   session[:cart_id] = @cart.id
+  protect_from_forgery with: :exception
+
+  helper_method :current_cart
+
+  private
+
+  def current_cart
+    @cart || set_cart # return @cart if @cart exist, or call set_cart
+  end
+
+  def set_cart
+
+    if session[:cart_id]
+      @cart = Cart.find_by(id: session[:cart_id])
+    end
+
+    @cart ||= Cart.create
+
+    session[:cart_id] = @cart.id
+    @cart
+  end
 end
+
+
 private
 
   def authenticate_admin
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,11 @@ class ProductsController < ApplicationController
     @products = Product.find(params[:id])
   end
   
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_cart_item(@product)
+
+    redirect_to root_path
+  end
+  
 end
