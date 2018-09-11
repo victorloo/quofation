c16d234432f607799182bf13312041a0852654e1
victorloo <ila9970@gmail.com>
2018-09-12 03:31:31 +0800

    Built prototype of album index page

--- app/controllers/fittingphotoes_controller.rb
+++ app/controllers/fittingphotoes_controller.rb
@@ -1,2 +1,8 @@
 class FittingphotoesController < ApplicationController
+  
+  def index
+    @product = Product.find(params[:thirtyday_id])
+    @photoes = @product.fitting_photoes
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-
-
   resources :designers, only: [:index, :show] do
     resources :products, only: [:index, :show]
   end
   root "designers#index"
-
 
   resources :thirtydays, only: [:index, :show] do
     collection do
       get :designers, to: "thirtydays#designers"
       get :products, to: "thirtydays#products"
     end
+    resources :fittingphotoes, path: 'album', only: [:index, :show]
   end
-
-
 
   #建立後台設計師CRUD
   namespace :admin do
@@ -28,7 +24,5 @@ Rails.application.routes.draw do
     resources :products
     root "products#index"
   end
-
-
 
 end
