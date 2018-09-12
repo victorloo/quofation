9b27b2a40c24a33e8505b26161c5285a5d22c68b
victorloo <ila9970@gmail.com>
2018-09-12 15:48:54 +0800

    add user name to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
 private
 
@@ -7,6 +9,12 @@ private
       flash[:alert] = "Not allow!"
       redirect_to root_path
     end
+  end
+
+  protected
+   
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates_presence_of :name
+
   has_one :designer
   has_many :fitting_photos
   has_many :discussions
