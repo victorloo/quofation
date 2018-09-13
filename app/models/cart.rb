1f478acfa81957df95471c4508c509920e35b8fe
jackal <hata.s520@gmail.com>
2018-09-13 16:04:55 +0800

    cart item and cart,product ralationship

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
 
   def add_cart_item(product)
     existing_item = self.cart_items.find_by( product: product )
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :designer
   has_many :fitting_photos
 
+  has_many :cart_items, dependent: :destroy
+  has_many :carts, through: :cart_items
 end
