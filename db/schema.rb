45e556044700fd5a12e99d9d225dba5c8998947f
gasper <bricktowntw@gmail.com>
2018-09-14 16:20:45 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_073239) do
+ActiveRecord::Schema.define(version: 2018_09_14_081828) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2018_09_12_073239) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -60,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_073239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "designer_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
