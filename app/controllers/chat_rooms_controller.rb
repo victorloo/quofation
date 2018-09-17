4a287c68de63305db7ef0d8d9efa023e330a6fc3
victorloo <ila9970@gmail.com>
2018-09-17 18:00:36 +0800

    Built client side of chatroom#show

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -6,6 +6,7 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
   
   def new
