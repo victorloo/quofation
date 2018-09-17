46efdf8e31e1b6032018f1d258987018e489e368
victorloo <ila9970@gmail.com>
2018-09-17 18:06:40 +0800

    Server side of chatroom

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
 
   validates :body, presence: true, length: {minimum: 2, maximum: 1000}
 
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+
 end
