1f932eab03adf0b8ec9de8adffe5a4f6da1c17fb
gasper <bricktowntw@gmail.com>
2018-09-17 23:02:02 +0800

    fix product image

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -57,7 +57,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :description, :image, photos_attributes: [:image])
+    params.require(:product).permit(:name, :price, :description, :image, photos_attributes: [:image], :category_id)
   end
 
   def set_product
