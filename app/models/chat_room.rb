6b9c5b503f27fc5245775272daa44a174930034e
victorloo <ila9970@gmail.com>
2018-09-17 17:37:30 +0800

    Create model: Message and setup association between Message, ChatRoom and User

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,3 +1,4 @@
 class ChatRoom < ApplicationRecord
   belongs_to :fitting_photo
+  has_many :messages, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_091915) do
+ActiveRecord::Schema.define(version: 2018_09_17_093435) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -65,6 +65,16 @@ ActiveRecord::Schema.define(version: 2018_09_17_091915) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "chat_room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
