8e9b5833e502055979f8467fe245a5ae2a06c658
蔡忠翊 <mac@caizhongyide-MacBook-Pro.local>
2018-09-17 21:03:22 +0800

    user can view his order on order index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,14 @@ class ApplicationController < ActionController::Base
     @cart
   end
 
+  def after_sign_in_path_for(resource)
+    if session[:new_order_data].present?
+      @cart = Cart.find(session[:cart_id])
+      cart_path(@cart)
+    else
+      super
+    end
+  end
 
   def authenticate_admin
     unless current_user.admin?
