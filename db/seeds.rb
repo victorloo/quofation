5d4bcf3d104a68ebf0944eb098d92a829adb21e3
gasper <bricktowntw@gmail.com>
2018-09-18 13:33:38 +0800

    set up seed data for default sizes

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,56 @@ category_list = [
   { name: "包包" }
 ]
 
+
+# Size
+
+Size.destroy_all
+
+size_list = [
+  { name: "S" },
+  { name: "M" },
+  { name: "L" },
+  { name: "XL" },
+  { name: "XXL" },
+  { name: "XXL" },
+  { name: "XXXL" },
+  { name: "USA 5"},
+  { name: "USA 5.5" },
+  { name: "USA 6" },
+  { name: "USA 6.5" },
+  { name: "USA 7" },
+  { name: "USA 7.5" },
+  { name: "USA 8" },
+  { name: "USA 8.5" },
+  { name: "USA 9"},
+  { name: "USA 9.5" },
+  { name: "USA 10" },
+  { name: "USA 10.5" },
+  { name: "USA 11" },
+  { name: "USA 11.5" },
+  { name: "USA 12" },
+  { name: "USA 12.5" },
+  { name: "USA 13"},
+  { name: "22 CM"},
+  { name: "24 CM" },
+  { name: "26 CM" },
+  { name: "28 CM" },
+  { name: "30 CM" },
+  { name: "32 CM" },
+  { name: "34 CM" },
+  { name: "36 CM" },
+  { name: "38 CM"},
+  { name: "40 CM" },
+  { name: "42 CM" },
+  { name: "44 CM" },
+  { name: "46 CM" }
+]
+
+size_list.each do |size|
+  Size.create( name: size[:name] )
+end
+puts "Size created!"
+
 User.create(
   email: "root@example.com",
   password: "12345678",
