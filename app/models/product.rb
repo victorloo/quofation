c50af50e6a2439182e20f345dbcb3f63068636c1
gasper <bricktowntw@gmail.com>
2018-09-18 15:05:09 +0800

    link size and product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   belongs_to :designer
   has_many :fitting_photos
   belongs_to :category, optional: true
+  belongs_to :size, optional: true
   has_many :comments, dependent: :destroy
   has_many :comment_users, through: :comments
   has_many :cart_items, dependent: :destroy
