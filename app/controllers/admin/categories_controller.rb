22cd980bb57a2290592ee985eb11847c73821006
gasper <bricktowntw@gmail.com>
2018-09-18 10:02:35 +0800

    add form on index page for creating new category and show errors message on form when validation fails

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,7 +6,27 @@ class Admin::CategoriesController < ApplicationController
   def index
     @designer = Designer.find(params[:designer_id])
     @categories = Category.all
+    @category = Category.new
   end
 
+  def create
+   @designer = Designer.find(params[:designer_id])
+   @category = Category.new(category_params)
+   if @category.save
+     flash[:notice] = "category was successfully created"
+     redirect_to admin_designer_path(params[:designer_id])
+   else
+     @categories = Category.all
+     render :index
+   end
+  end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
+
+
+
