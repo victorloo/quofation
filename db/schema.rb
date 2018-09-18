4f95472f4a246aeae75cce5779c56e0fec048f0a
gasper <bricktowntw@gmail.com>
2018-09-18 16:55:12 +0800

    create inventory model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_080039) do
+ActiveRecord::Schema.define(version: 2018_09_18_084806) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -79,6 +79,12 @@ ActiveRecord::Schema.define(version: 2018_09_18_080039) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -123,6 +129,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_080039) do
     t.integer "category_id"
     t.integer "size_id"
     t.integer "color_id"
+    t.integer "inventory_id"
   end
 
   create_table "sizes", force: :cascade do |t|
