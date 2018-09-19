d6e75ebafe125a7e26cc6adce6a511a59fca3994
蔡忠翊 <mac@caizhongyide-MacBook-Pro.local>
2018-09-19 15:54:28 +0800

    send data to spgateway

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -47,6 +47,38 @@ class OrdersController < ApplicationController
         order_id: @order.id,
         amount: @order.amount
       )
+
+      # get params string
+      spgateway_data = {
+        MerchantID: "MS33418458",
+        Version: 1.4,
+        RespondType: "JSON",
+        TimeStamp: Time.now.to_i,
+        MerchantOrderNo: "#{@payment.id}",
+        Amt: @order.amount,
+        ItemDesc: @order.name,
+        Email: @order.user.email,
+        LoginType: 0
+      }.to_query
+
+      hash_key = ""
+      hash_iv = ""
+
+      cipher = OpenSSL::Cipher::AES256.new(:CBC)
+      cipher.encrypt
+      cipher.key = hash_key
+      cipher.iv  = hash_iv
+      encrypted = cipher.update(spgateway_data) + cipher.final
+      aes = encrypted.unpack('H*').first
+
+      str = "HashKey=#{hash_key}&#{aes}&HashIV=#{hash_iv}"
+      sha = Digest::SHA256.hexdigest(str).upcase
+
+      @merchant_id = ""
+      @trade_info = aes
+      @trade_sha = sha
+      @version = "1.4"
+
       render layout: false
     end
   end
