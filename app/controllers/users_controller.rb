94c67e3bad7c4f4c1660ba503fdd9d96a589c3c4
victorloo <ila9970@gmail.com>
2018-09-25 17:57:50 +0800

    Rebuild structure of User's show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    redirect_to user_path(current_user), notice:"請不要去別人的頁面！" if @user.id != current_user.id
+
+    @orders = @user.orders.order(created_at: :desc).limit(3)
+    @chatrooms = @user.chat_rooms.order(created_at: :desc).limit(3)
+    @des_chars = current_user.designer.chat_rooms if current_user.role == "designer"
   end
 end
