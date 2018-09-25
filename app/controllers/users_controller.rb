393fe2de23b4f07dca39feb36d971096b5127cec
victorloo <ila9970@gmail.com>
2018-09-25 18:36:46 +0800

    Move .limit to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to user_path(current_user), notice:"請不要去別人的頁面！" if @user.id != current_user.id
 
-    @orders = @user.orders.order(created_at: :desc).limit(3)
-    @chatrooms = @user.chat_rooms.order(created_at: :desc).limit(3)
-    @des_chars = current_user.designer.chat_rooms if current_user.role == "designer"
+    @orders = @user.orders.order(created_at: :desc)
+    @chatrooms = @user.chat_rooms.order(created_at: :desc)
+    @des_chars = current_user.designer.chat_rooms.order(created_at: :desc) if current_user.role == "designer"
   end
 end
