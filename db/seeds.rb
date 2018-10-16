8199216c8229a9ef3e137a9b00145ffbdc933449
victorloo <ila9970@gmail.com>
2018-10-16 20:59:47 +0800

    Remove Inventory_list in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -124,47 +124,7 @@ color_list.each do |color|
 end
 puts "Category created!"
 
-# Inventory
-
-Inventory.destroy_all
-
-inventory_list = [
-  { name: "1" },
-  { name: "2" },
-  { name: "3" },
-  { name: "4" },
-  { name: "5" },
-  { name: "6" },
-  { name: "7" },
-  { name: "8" },
-  { name: "9" },
-  { name: "10" },
-  { name: "11" },
-  { name: "12" },
-  { name: "13" },
-  { name: "14" },
-  { name: "15" },
-  { name: "16" },
-  { name: "17" },
-  { name: "18" },
-  { name: "19" },
-  { name: "20" },
-  { name: "21" },
-  { name: "22" },
-  { name: "23" },
-  { name: "24" },
-  { name: "25" },
-  { name: "26" },
-  { name: "27" },
-  { name: "28" },
-  { name: "29" },
-  { name: "30" }
-]
-
-inventory_list.each do |inventory|
-  Inventory.create( name: inventory[:name] )
-end
-puts "Inventory created!"
+# Admin
 
 User.create(
   email: "root@example.com",
