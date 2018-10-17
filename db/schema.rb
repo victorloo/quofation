4653e17b9894a270ec55d5370e88cd8c70e8a01b
victorloo <ila9970@gmail.com>
2018-10-18 02:51:21 +0800

    Add more FK to table: cart_item and order_item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_124423) do
+ActiveRecord::Schema.define(version: 2018_10_17_184159) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2018_10_16_124423) do
     t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
+    t.integer "color_id"
+    t.integer "inventory_id"
   end
 
   create_table "carts", force: :cascade do |t|
@@ -93,6 +96,8 @@ ActiveRecord::Schema.define(version: 2018_10_16_124423) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
+    t.integer "color_id"
   end
 
   create_table "orders", force: :cascade do |t|
