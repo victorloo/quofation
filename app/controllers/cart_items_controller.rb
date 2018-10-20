21a7a7aa09cd7f1d7e044ca91d97fd20d5c699c4
victorloo <ila9970@gmail.com>
2018-10-20 14:45:03 +0800

    Add method: create to cart_items controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,22 @@
 class CartItemsController < ApplicationController
+  def create
+    @cart_item = current_cart.cart_items.build(cart_item_params)
+    @size = Size.find(cart_item_params[:size_name])
+    @cart_item.size_name = @size.name
+    @invnetory = Inventory.where(color_id: cart_item_params[:color_id], size_id: @size.id, product_id: cart_item_params[:product_id]).first
+    @cart_item.inventory_id = @invnetory.id
+    if @cart_item.save
+      flash[:notice] = "Product was successfully added to cart."
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = "Oops, there are some error."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:cart_id, :product_id, :quantity, :size_name, :color_id, :color_name, :inventory_id)
+  end
 end
