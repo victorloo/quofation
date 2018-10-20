df9f3b792f714389aabdb010162d9f4ed67ced92
victorloo <ila9970@gmail.com>
2018-10-20 14:36:34 +0800

    Let automatically_add_attributes as private method

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -26,6 +26,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.designer_id = params[:designer_id]
     if @product.save
+      automatically_add_attributes
       flash[:notice] = "Product was successfully created"
       redirect_to admin_designer_path(params[:designer_id])
     else
@@ -38,12 +39,7 @@ class Admin::ProductsController < ApplicationController
     @designer = Designer.find(params[:designer_id])
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      @product.inventories.each do |inventory|
-        inventory.update!(
-          color_name: inventory.color.name,
-          size_name: inventory.size.name
-        )
-      end
+      automatically_add_attributes
       flash[:notice] = "product was successfully updated"
       redirect_to admin_designer_path(params[:designer_id])
     else
@@ -72,6 +68,15 @@ class Admin::ProductsController < ApplicationController
   def set_product
     @designer = Designer.find(params[:designer_id])
     @products = Product.find(params[:id])
+  end
+
+  def automatically_add_attributes
+    @product.inventories.each do |inventory|
+      inventory.update!(
+        color_name: inventory.color.name,
+        size_name: inventory.size.name
+      )
+    end
   end
 
 end
