48df9aca444b3a6e8452dc29126cbf6b7b557397
victorloo <ila9970@gmail.com>
2018-10-24 02:21:10 +0800

    Add designer/product/comment to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :designers, only: [:index, :show] do
-    resources :products, only: [:index, :show]
+    resources :products, only: [:index, :show] do
+      resources :comments, only: [:create, :destroy]
+    end
   end
   resources :products, only: [:index, :show] do
     post :add_to_cart, on: :member
