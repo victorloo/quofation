e21c41fbff500630ab2c8609ba2600c62e8d61e2
victorloo <ila9970@gmail.com>
2018-10-25 04:08:23 +0800

    Admin can toggle SHIPPING_STATUS of order_item

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrdersController < ApplicationController
+  before_action :set_order, except: [:index, :update_order_item]
   def index
     @orders = Order.all
   end
@@ -10,11 +11,8 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
-      if @order.shipping_status == "shipped"
-        UserMailer.notify_order_shipped(@order).deliver_now
-      end
       if @order.payment_status == "paid"
-        UserMailer.notify_order_paid(@order).deliver_now
+        #UserMailer.notify_order_paid(@order).deliver_now
       end
       redirect_to admin_orders_path, notice: "Order updated"
     else
@@ -22,11 +20,32 @@ class Admin::OrdersController < ApplicationController
       render "admin/orders/edit"
     end
   end
+
+  def update_order_item
+    @order_item = OrderItem.find(params[:id])
+    if @order_item.update(order_item_params)
+      if @order_item.shipping_status == "shipped"
+        #UserMailer.notify_order_shipped(@order_item.order).deliver_now
+      end
+      redirect_to admin_order_path(@order_item.order), notice: "Order Item updated"
+    else
+      flash.now[:alert] = @order_item.errors.full_messages.to_sentence
+      redirect_to admin_order_path(@order_item.order), alert: "There are some errors."
+    end
+  end
    
   private
 
   def order_params
-    params.require(:order).permit(:payment_status, :shipping_status)
+    params.require(:order).permit(:payment_status)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_item_params
+    params.require(:order_item).permit(:shipping_status)
   end
 
 end
