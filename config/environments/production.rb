0b31bd3152bcb59808de251ad6a6e22313e4f2cc
victorloo <ila9970@gmail.com>
2018-10-26 14:56:22 +0800

    Amend subdomain for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.allowed_request_origins = ['https://quofation.herokuapp.com/', 'https://quofation.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://pre-release-quofation.herokuapp.com/', 'https://pre-release-quofation.herokuapp.com/']
   config.action_cable.url = 'wss://quofation.herokuapp.com/cable'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
