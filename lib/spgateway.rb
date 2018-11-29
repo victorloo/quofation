ff68ddee96643bcb1f5961f5ca243214cbfa4f36
victorloo <ila9970@gmail.com>
2018-11-30 01:20:55 +0800

    Close option of atm

--- lib/spgateway.rb
+++ lib/spgateway.rb
@@ -30,16 +30,13 @@ class Spgateway
       Email: @payment.order.user.email,
       LoginType: 0,
       CREDIT: 0,
-      WEBATM: 0,
-      VACC: 0
+      WEBATM: 0
     }
     case @payment.payment_method
       when "Credit"
         spgateway_data.merge!( :CREDIT => 1 )
       when "WebATM"
         spgateway_data.merge!( :WEBATM => 1 )
-      when "ATM"
-        spgateway_data.merge!( :VACC => 1, :ExpireDate => @payment.deadline.strftime("%Y%m%d") )
     end
     
     trade_info = self.encrypt(spgateway_data)
